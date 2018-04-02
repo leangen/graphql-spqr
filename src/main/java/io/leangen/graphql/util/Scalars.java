6da7efc8140924e00c75c3849e6ab5cafb8aca47
Jestan Nirojan <jestannirojan@gmail.com>
2018-04-02 15:44:46 +0530

    Fixed GraphQLIgnore and ObjectScalar parsing error (#88)
    * GraphQLIgnore on types ignores the type in auto-discovery
    
    * Fixed ObjectScalar parsing error (for example 'IntValue{value=1}' is not a valid 'ObjectScalar')
    
    * Fixed enum scalar parsing error

--- src/main/java/io/leangen/graphql/util/Scalars.java
+++ src/main/java/io/leangen/graphql/util/Scalars.java
@@ -174,8 +174,8 @@ public class Scalars {
 
             @Override
             public Object parseLiteral(Object input) {
-                if (!(input instanceof ObjectValue)) return null;
-                return parseFieldValue(((ObjectValue) input));
+                if (!(input instanceof Value)) return null;
+                return parseFieldValue(((Value) input));
             }
 
             private Object parseFieldValue(Value value) {
@@ -192,7 +192,7 @@ public class Scalars {
                     return ((BooleanValue) value).isValue();
                 }
                 if (value instanceof EnumValue) {
-                    ((EnumValue) value).getName();
+                    return ((EnumValue) value).getName();
                 }
                 if (value instanceof ArrayValue) {
                     return ((ArrayValue) value).getValues().stream()
