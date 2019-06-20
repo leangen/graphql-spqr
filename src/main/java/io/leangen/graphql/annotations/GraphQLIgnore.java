90b4a016249e6c21766a170880b7ae348cab74e6
Viktor Nyström <vnystrom@gmail.com>
2019-06-20 12:01:36 +0200

    add reason field to GraphQLIgnore annotation
    add a quick field to document why this specific annotation is placed on a field.

--- src/main/java/io/leangen/graphql/annotations/GraphQLIgnore.java
+++ src/main/java/io/leangen/graphql/annotations/GraphQLIgnore.java
@@ -12,4 +12,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
 public @interface GraphQLIgnore {
+  
+  String reason() default "";
 }
