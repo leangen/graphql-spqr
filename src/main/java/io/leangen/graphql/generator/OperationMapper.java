d8589800fed778bc7c4c3198e7bdf8a7d538ca4a
Leo osang <leo.osang@gmail.com>
2020-07-02 21:42:46 +1000

    Make clientMutationId optional

--- src/main/java/io/leangen/graphql/generator/OperationMapper.java
+++ src/main/java/io/leangen/graphql/generator/OperationMapper.java
@@ -376,7 +376,7 @@ public class OperationMapper {
                 .name(mutation.getName() + "Input")
                 .field(newInputObjectField()
                         .name(CLIENT_MUTATION_ID)
-                        .type(new GraphQLNonNull(GraphQLString)))
+                        .type(GraphQLString))
                 .fields(inputFields)
                 .build();
 
@@ -384,7 +384,7 @@ public class OperationMapper {
                 .name(payloadTypeName)
                 .field(newFieldDefinition()
                         .name(CLIENT_MUTATION_ID)
-                        .type(new GraphQLNonNull(GraphQLString)))
+                        .type(GraphQLString))
                 .fields(outputFields)
                 .build();
 
